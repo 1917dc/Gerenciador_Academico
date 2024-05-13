a8d708410da20cad50314ecc60f24fcd049878d5
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-05-13 15:01:30 -0300

    leitura de arquivos

--- Gerenciador_Academico/src/model/WriteFile.java
+++ Gerenciador_Academico/src/model/WriteFile.java
@@ -1,16 +1,33 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 public class WriteFile {
+    public static void main(String[] args) {
+        WriteFile writeFile = new WriteFile();
+        writeFile.writeAluno("12345566");
+    }
     private ReadFile readFile = new ReadFile();
     public void writeAluno(String aluno) {
-        Aluno alunoWrite = null;
-        for(Aluno a : readFile.getAllAlunos()) {
-            if (a.getCpf().equals(aluno)) {
-                alunoWrite = a;
-            }
+//        Aluno alunoWrite = null;
+//        for(Aluno a : readFile.getAllAlunos()) {
+//            if (a.getCpf().equals(aluno)) {
+//                alunoWrite = a;
+//            }
+//        }
+//        readFile.getAllAlunos().removeIf(a -> a.getCpf().equals(aluno));
+//        readFile.getAllAlunos().add(alunoWrite);
+//        System.out.println(readFile.getAllAlunos().get(2).toString());
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("/home/lua/Documents/GitHub/Gerenciador_Academico/Gerenciador_Academico/src/teste.txt");
+            fos.write("teste".getBytes("UTF-8"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        readFile.getAllAlunos().removeIf(a -> a.getCpf().equals(aluno));
-        readFile.getAllAlunos().add(alunoWrite);
-        System.out.println(readFile.getAllAlunos().get(2).toString());
     }
 }
